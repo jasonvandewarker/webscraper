83f9dab2b3f3d511c20473bc63e7962b699ca15a
jasonvandewarker <jasonvandewarker@gmail.com>
2019-05-14 21:40:28 -0700

    Just have to make it accept an array.

---
+++ server.js
@@ -0,0 +1,73 @@
+const rp = require('request-promise');
+const fs = require('fs');
+var express = require('express');
+var app = express();
+var router = express.Router();
+
+var port = process.env.PORT || 8080
+
+app.use('/api', router);
+
+router.use(function(req, res, next) {
+    console.log('Something is happening.');
+    next(); // make sure we go to the next routes and don't stop here
+});
+
+router.get('/getScraperResults', function(req, res) {
+    //res.json({ message: 'hooray! welcome to our api!' });
+try {
+    // console.log('this is req' + req)
+    console.log(req.query.url)
+
+    rp(req.query.url)
+        .then(function(html){
+            //success!
+            console.log(html);
+            res.status(200).json({message: html})
+        })
+        .catch(function(err){
+            //handle error
+        });
+
+
+
+    // for (let i = 0; i < req.query.url.length; i++) {
+    //
+    //     rp(req.query.url[i])
+    //         .then(function(html){
+    //             function myWrite(html) {
+    //                 fs.writeFile(`./scrape-results/output-${i}.txt`, html, function (err) {
+    //                     if (err) {
+    //                         console.log(err)
+    //                     }
+    //                 })
+    //             }
+    //             myWrite(html)
+    //
+    //             res.json({message: "great success!"})
+    //         }).catch(function(err){
+    //         console.log(err)
+    //     });
+    // }
+
+
+
+
+
+
+}catch(err) {
+    res.status(500).json({error: err})
+}
+
+
+
+
+
+
+});
+
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
