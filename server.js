ebd77c59d8bc86e9212d27e5fddc2a198c663dd1
jasonvandewarker <jasonvandewarker@gmail.com>
2019-05-17 14:28:22 -0700

    pushing up final version.

--- server.js
+++ server.js
@@ -11,33 +11,28 @@ app.use(cors());
 app.use('/api', router);
 
 router.use(function(req, res, next) {
-    console.log('Something is happening.');
-    next();
+  console.log('Something is happening.');
+  next();
 });
 
 router.get('/getScraperResults', function(req, res) {
-try {
+  try {
     let websiteArray = req.query.url.split(',')
     for (let i = 0; i < websiteArray.length; i++) {
-        rp(websiteArray[i])
-            .then(function(html){
-                function myWrite(html) {
-                    fs.writeFile(`./scrape-results/output-${i}.txt`, html, function (err) {
-                        if (err) {
-                            console.log(err)
-                        }
+      rp(websiteArray[i])
+        .then(function(html){
+          function myWrite(html) {
+            fs.writeFile(`./scrape-results/output-${i}.txt`, html, function (err) {
+                if (err) {console.log(err)}
                     })
-                }
-                myWrite(html)
-            })
-        }
-    res.status(200).json({message: "success"})
-}
-catch(err) {
+                }myWrite(html)
+          })
+        }  res.status(200).json({message: "success"})
+  }
+  catch(err) {
     res.status(404).json({error: err})
     res.status(500).json({error: err})
-    console.log(err)
-}
+    console.log(err)  }
 });
 
 app.listen(port);
